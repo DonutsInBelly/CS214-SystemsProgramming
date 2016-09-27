7c8bce6d8bdec86d7804b4e0130fc84ba20de847
Carlin Au <carlinau@gmail.com>
2016-09-27 16:02:11 -0400

    just basic stuff so far

--- assignment1/mymalloc.c
+++ assignment1/mymalloc.c
@@ -1,1 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
+
+static char myBlock[5000];
+
+void malloc(int size) {
+  
+}
---
+++ assignment1/test.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef TEST_H
+#define TEST_H
+
+typedef struct obj{
+  char letter;
+  int num1;
+  double num2;
+}obj;
+
+#endif
