1b5a9b592fcf349d29a582a145eb4b1d6d95f46a
Benjamin De Brasi <bd241@null.cs.rutgers.edu>
2016-10-11 00:36:53 -0400

    Initial dabbling

--- assignment1/mymalloc.c
+++ assignment1/mymalloc.c
@@ -3,6 +3,44 @@
 
 static char myBlock[5000];
 
-void malloc(int size) {
-  
+void malloc(size_t size) {
+	  
+	if(size == 0){
+		return null;
+	}
+	
+	if(size > 5000 - /*size of metadata*/){
+		return null;
+	}
+
+	int i = 0;
+
+	while(arr[i] > 0 || (arr[i] < 0 && arr[i] < -size) || i > 4999){
+		i = arr[i] + i;
+	}
+
+	if( (i + /*size of metadata*/ + size) > 4999){
+		return null;
+	}
+
+	//Loop through each array index and put each bit into thier indices. 
+	int *num = (int*) (&arr[i]);
+	num = size;
+
+	return int *num + 4;
+	
+}
+
+void free(void *ptr){
+	
+	int *ptrInt = (int*) (&ptr);
+	ptrInt--;
+
+	
+
+	return;
+	//if unallocated, error
+	//if not address, error
+	//
+
 }
