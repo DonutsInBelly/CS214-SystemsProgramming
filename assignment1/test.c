9e1e005cde7264c57d0099d0df68b18f17031fec
Carlin Au <carlinau@gmail.com>
2016-10-13 15:37:34 -0400

    malloc doesnt work on second malloc

--- assignment1/test.c
+++ assignment1/test.c
@@ -4,8 +4,14 @@
 #include "mymalloc.h"
 
 int main(int argc, char** argv) {
-  struct obj* test = malloc(sizeof(struct obj));
-  test->a = 9;
-  test->b = 'l';
-  printf("Number: %d\nCharacter: %c\n", test->a, test->b);
+  struct obj1* test1 = mymalloc(sizeof(struct obj1));
+  test1->a = 9;
+  test1->b = 'l';
+  printf("Number: %d\nCharacter: %c\n", test1->a, test1->b);
+
+  struct obj2* test2 = mymalloc(sizeof(struct obj2));
+  test2->b = 9.068;
+  test2->c = 'p';
+  test2->a = 8;
+  printf("a= %d\nb= %ld\nc= %c\n", test2->a, test2->b, test2->c);
 }
--- assignment1/test.h
+++ assignment1/test.h
@@ -4,9 +4,15 @@
 #ifndef TEST_H
 #define TEST_H
 
-typedef struct obj{
+typedef struct obj1{
   int a;
   char b;
-}obj;
+}obj1;
+
+typedef struct obj2{
+  int a;
+  double b;
+  char c;
+}obj2;
 
 #endif
