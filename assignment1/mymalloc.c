149917d06ea76a87c48b3d04b0fa9a0d411aa482
Carlin Au <carlinau@gmail.com>
2016-10-14 00:14:44 -0400

    Malloc works on second malloc

--- assignment1/mymalloc.c
+++ assignment1/mymalloc.c
@@ -22,13 +22,23 @@ void *mymalloc(size_t size) {
 		return NULL;
 	}
 
+	size_t i = 0;
 	// search for open space in block
-	for (size_t i = 0; i < MAX_SIZE; i++) {
+	while (i < MAX_SIZE) {
+		printf("Current Block Index: %d\n", i);
+		// if index is non-negative and has a value, this slot is allocated
+		if(myBlock[i]>0) {
+			i = myBlock[i];
+			printf("Moving to next block at address: 0x%02X\n", &myBlock[i]);
+			continue;
+		}
 		// if index available
 		// -1 denotes first allocation
 		if (myBlock[i]==-1) {
-			myBlock[i] = size;
+			// puts index of next Metadata location in this index
+			myBlock[i] = i + 4 + size;
 			lastAssigned = i;
+			printf("Metadata Location: 0x%02X\nAllocating %d bits at address: 0x%02X, with index: %d\nNext block is: 0x%02X with index %d\n", &myBlock[i], size, &myBlock[i+4], i+4, &myBlock[i+4+size], i+4+size);
 			return &myBlock[i+4];
 		}
 
--- assignment1/test.c
+++ assignment1/test.c
@@ -5,13 +5,16 @@
 
 int main(int argc, char** argv) {
   struct obj1* test1 = mymalloc(sizeof(struct obj1));
-  test1->a = 9;
-  test1->b = 'l';
+  test1->a = 90;
+  test1->b = 't';
+  printf("Address of test1: 0x%02X\n", test1);
   printf("Number: %d\nCharacter: %c\n", test1->a, test1->b);
+  //printf("Field a is at: 0x%02X\nField b is at: 0x%02X\n", );
 
   struct obj2* test2 = mymalloc(sizeof(struct obj2));
+  test2->a = 8;
   test2->b = 9.068;
   test2->c = 'p';
-  test2->a = 8;
-  printf("a= %d\nb= %ld\nc= %c\n", test2->a, test2->b, test2->c);
+  printf("Address of test2: 0x%02X\n", test2);
+  printf("a= %d\nb= %lf\nc= %c\n", test2->a, test2->b, test2->c);
 }
