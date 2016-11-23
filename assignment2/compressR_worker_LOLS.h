ebb8b0892bf49e68194891e6784ad260df110b5f
Carlin Au <carlinau@gmail.com>
2016-11-22 23:18:06 -0500

    Processes still broken ish

--- assignment2/compressR_worker_LOLS.h
+++ assignment2/compressR_worker_LOLS.h
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lols.h"
+#include "utils.h"
 
-##ifndef compressR_worker_LOLS_H
+#ifndef compressR_worker_LOLS_H
 #define compressR_worker_LOLS_H
 
 void worker(FileData *);
